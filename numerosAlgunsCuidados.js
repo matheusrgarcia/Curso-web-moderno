022cebccb788467678d0c37ad1d9d6b6b13c3d1a
Matheus <matheus_rock14@hotmail.com>
2019-03-06 19:29:17 -0300

    Usual commit

---
+++ numerosAlgunsCuidados.js
@@ -0,0 +1,14 @@
+console.log(7 / 0.0001)
+console.log(7 / 0) 
+
+
+console.log('10.2' / 2)
+console.log("Show" / 2)
+
+console.log(0.7 + 0.1)
+let fixedValue = 0.7 + 0.1
+
+console.log('Possible way to fix this ' + fixedValue.toFixed(1))
+
+//console.log(10.toString()) // Doesn't work
+console.log(Number(10).toString())
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+console.log(7 / 0.0001)
+console.log('10.2' / 2)
+console.log("Show" / 2)
+console.log(0.6 + 0.1)
